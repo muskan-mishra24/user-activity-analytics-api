a862782cc9b7a476386a1bec463fa9884750ffc9
Muskan Mishra <muskanm2405@gmail.com>
2026-06-25 02:16:25 +0530

    Delete Backend directory

--- Backend/src/routes/analyticsRoutes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const { getDailyLogs, getUserActivity, getStats} = require('../controllers/analyticsController');
-
-router.get('/daily', getDailyLogs);
-router.get('/users', getUserActivity);
-router.get("/stats", getStats);
-
-module.exports = router;
