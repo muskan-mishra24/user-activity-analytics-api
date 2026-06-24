283559116a09ef442c12e2e9c69c1de5020f786d
Muskan Mishra <muskanm2405@gmail.com>
2026-06-25 02:26:14 +0530

    Delete Frontend directory

--- Frontend/src/services/logService.js
+++
@@ -1,7 +0,0 @@
-import API from "../api/axios";
-
-export const createLog = (action) =>
-  API.post("/logs", { action });
-
-export const getLogs = (page = 1, action = "") =>
-  API.get(`/logs?page=${page}&action=${action}`);
