f1c0dad64c47006bbe027e8238cb388f8d41d5fa
Muskan Mishra <muskanm2405@gmail.com>
2026-08-02 00:37:57 +0530

    src

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://user-activity-analytics-fr-git-d13a00-muskan-mishra24s-projects.vercel.app/"
+      "https://user-activity-analytics-frontendmm.vercel.app/"
     ],
     credentials: true,
   })
